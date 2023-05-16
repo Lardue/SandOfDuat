e5489620c690e38bcf52d0111ad24b1eb212c433
Lara Düsterhöft <Lara.unimail@gmail.com>
2023-05-16 18:58:25 +0200

    ADD: First Person Character + TriggerEvent Script

---
+++ SandOfDuat/Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SandOfDuat
+{
+    public class TriggerEvent : MonoBehaviour
+    {
+        public UnityEvent triggerEnterEvent;
+        public UnityEvent triggerStayEvent;
+        public UnityEvent triggerExitEvent;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                triggerEnterEvent?.Invoke();
+            }
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player") && Input.GetKeyDown("e"))
+            {
+                triggerStayEvent?.Invoke();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                triggerExitEvent?.Invoke();
+            }
+        }
+    }
+}
