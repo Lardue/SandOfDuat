0ea1de20f91009afaa0f329433cb625f66c95ce9
Lara Düsterhöft <Lara.unimail@gmail.com>
2023-06-04 15:27:56 +0200

    ADD: FadeOut Black and White

--- SandOfDuat/Assets/Scripts/WeighHeart.cs
+++ SandOfDuat/Assets/Scripts/WeighHeart.cs
@@ -6,10 +6,22 @@ namespace SandOfDuat
 {
     public class WeighHeart : MonoBehaviour
     {
+        //Weighing Variables
         private bool ritualStarted;
-        public int RadNum;
+        private int RadNum;
 
-        public void weighingHeart()
+        //Fade Variables
+        public float fadeDuration = 2;
+        public Color fadeColorAlive;
+        public Color fadeColorDead;
+        private Renderer rend;
+
+        private void Start()
+        {
+            rend = GetComponent<Renderer>();
+        }
+
+        public void WeighingHeart()
         {
             if (!ritualStarted)
             {
@@ -19,15 +31,55 @@ namespace SandOfDuat
                 if (RadNum == 0)
                 {
                     Debug.Log("You died AGAIN! Bye bye...");
+                    FadeOut(fadeColorDead);
                 } 
                 else if ( RadNum == 1)
                 {
                     Debug.Log("You're still part of the live, laugh, love community...");
-                } else
+                    FadeOut(fadeColorAlive);
+                } 
+                else
                 {
                     Debug.Log("Something went horribly wrong!");
                 }
             }
         }
+
+
+        public void FadeIn(Color fadeColor)
+        {
+            Fade(1, 0, fadeColor);
+        }
+
+        public void FadeOut(Color fadeColor)
+        {
+            Fade(0, 1, fadeColor);
+        }
+
+        public void Fade(float alphaIn, float alphaOut, Color fadeColor)
+        {
+            StartCoroutine(FadeRoutine(alphaIn, alphaOut, fadeColor));
+        }
+
+        public IEnumerator FadeRoutine(float alphaIn, float alphaOut, Color fadeColor)
+        {
+            float timer = 0; 
+            while(timer <= fadeDuration)
+            {
+                Color newColor = fadeColor;
+                newColor.a = Mathf.Lerp(alphaIn, alphaOut, timer/fadeDuration);
+
+                rend.material.SetColor("_Color", newColor);
+
+                timer += Time.deltaTime;
+                yield return null; 
+            }
+
+            Color newColor2 = fadeColor;
+            newColor2.a = alphaOut;
+
+            rend.material.SetColor("_Color", newColor2);
+        }
+
     }
 }
