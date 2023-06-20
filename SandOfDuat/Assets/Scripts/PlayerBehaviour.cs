424bd02760cfb59180dd7f89b639cbc302760242
Lara Düsterhöft <Lara.unimail@gmail.com>
2023-06-21 01:52:37 +0200

    ADD: Teleport Desktop Player

---
+++ SandOfDuat/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SandOfDuat
+{
+    public class PlayerBehaviour : MonoBehaviour
+    {
+        public GameObject teleportationTarget;
+        public GameObject player;
+
+        public void goTroughPortal()
+        {
+            player.transform.position = teleportationTarget.transform.position;
+        }
+    }
+}
