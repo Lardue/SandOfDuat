56758cc3d1a6335a4c602cc9c559f95683506832
rik-ahl <rik_ahl@uni-bremen.de>
2023-06-26 18:19:01 +0200

    CNG: Feather and core stuff for player

--- SandOfDuat/Assets/Scripts/PortalCamera.cs
+++ SandOfDuat/Assets/Scripts/PortalCamera.cs
@@ -40,7 +40,7 @@ namespace SandOfDuat
             //newPos.Set(0, newPos.y, newPos.z, newPos.z);
 
             Quaternion newRot = m.rotation;
-            newRot.Set(0, newRot.y, newRot.z, newRot.w);
+            newRot.Set(0, newRot.y, 0, newRot.w);
 
             portalCam.SetPositionAndRotation(newPos, newRot);
         }
