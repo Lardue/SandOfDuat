964c7b83ed347d4896e66435192ff3a70616c7c6
Lara Düsterhöft <Lara.unimail@gmail.com>
2023-07-04 22:44:30 +0200

    ADD: Restart Game

--- SandOfDuat/Assets/Scripts/WeighHeart.cs
+++ SandOfDuat/Assets/Scripts/WeighHeart.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace SandOfDuat
 {
@@ -12,15 +13,19 @@ namespace SandOfDuat
         private int RadNum;
 
         //Scale Variables 
-        [SerializeField] ScaleState scaleScript; 
+        [SerializeField] ScaleState scaleScript;
         public GameObject scale;
-        private Animator scaleAnimator; 
+        private Animator scaleAnimator;
 
         //Fade Variables
         public float fadeDuration = 2;
         public Color fadeColorAlive;
         public Color fadeColorDead;
         private Renderer rend;
+
+        //Sound Variables 
+        public AudioSource soundDeath;
+        public AudioSource soundAfterlife;
 
         private void Start()
         {
@@ -33,19 +38,19 @@ namespace SandOfDuat
         {
             if (!ritualStarted)
             {
-                ritualStarted = true; 
+                ritualStarted = true;
                 RadNum = Random.Range(0, 2);
-                
+
                 if (RadNum == 0)
                 {
                     Debug.Log("You died AGAIN! Bye bye...");
                     scaleAnimator.Play("Heavy_Right");
-                } 
+                }
                 else if (RadNum == 1)
                 {
                     Debug.Log("You're still part of the live, laugh, love community...");
                     scaleAnimator.Play("Heavy_Left");
-                } 
+                }
                 else
                 {
                     Debug.Log("Something went horribly wrong!");
@@ -53,27 +58,34 @@ namespace SandOfDuat
             }
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
-            if(ritualStarted && !ritualEnded)
+            if (ritualStarted && !ritualEnded)
             {
-                if(scaleScript.animationFinished)
+                if (scaleScript.animationFinished)
                 {
-                ritualEnded = true;
-
-                    if (RadNum == 0)
-                    {
-                        FadeOut(fadeColorDead);
-                    } 
-
-                    if (RadNum == 1)
-                    {
-                        FadeOut(fadeColorAlive);
-                    }
+                    ritualEnded = true;
+                    StartCoroutine(EndGame());
                 }
             }
         }
+       
+        private IEnumerator EndGame()
+        {
+            yield return new WaitForSeconds(3f);
 
+            if (RadNum == 0)
+            {
+                FadeOut(fadeColorDead);
+                StartCoroutine(PlayEndSound(5));
+            }
+
+            if (RadNum == 1)
+            {
+                FadeOut(fadeColorAlive);
+                StartCoroutine(PlayEndSound(5));
+            }
+        }
 
         public void FadeIn(Color fadeColor)
         {
@@ -92,16 +104,16 @@ namespace SandOfDuat
 
         public IEnumerator FadeRoutine(float alphaIn, float alphaOut, Color fadeColor)
         {
-            float timer = 0; 
-            while(timer <= fadeDuration)
+            float timer = 0;
+            while (timer <= fadeDuration)
             {
                 Color newColor = fadeColor;
-                newColor.a = Mathf.Lerp(alphaIn, alphaOut, timer/fadeDuration);
+                newColor.a = Mathf.Lerp(alphaIn, alphaOut, timer / fadeDuration);
 
                 rend.material.SetColor("_Color", newColor);
 
                 timer += Time.deltaTime;
-                yield return null; 
+                yield return null;
             }
 
             Color newColor2 = fadeColor;
@@ -110,9 +122,28 @@ namespace SandOfDuat
             rend.material.SetColor("_Color", newColor2);
         }
 
-        private IEnumerator WaitBeforeFade()
+        private IEnumerator PlayEndSound(float pSeconds)
         {
-            yield return new WaitForSeconds(3);
+            yield return new WaitForSeconds(pSeconds);
+
+            if (RadNum == 0)
+            {
+                soundDeath.Play();
+                StartCoroutine(ResetGame(40));
+            }
+
+            if (RadNum == 1)
+            {
+                soundAfterlife.Play();
+                StartCoroutine(ResetGame(40));
+            }
+        }
+
+        private IEnumerator ResetGame(float pSeconds)
+        {
+            yield return new WaitForSeconds(pSeconds);
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
 
     }
